7b506c379f2d47236696faaca999bac79f4caba2
Ayush Singh <ayush.rocks99@gmail.com>
2022-05-24 11:33:52 +0530

    First

---
+++ Assets/Pedestrian.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Pedestrian : MonoBehaviour
+{
+    NavMeshAgent Ag;
+    public Transform[] NPCPoints;
+    int r;
+    private void Start()
+    {
+        Ag = GetComponent<NavMeshAgent>();
+        r = Random.Range(0, 7);
+        Ag.SetDestination(NPCPoints[r].position);
+    }
+
+    void ReLoad()
+    {
+       r = Random.Range(0, 7);
+        Ag.SetDestination(NPCPoints[r].position);
+    }
+    private void Update()
+    {
+        Debug.Log(Ag.destination);
+        if (Vector3.Distance(NPCPoints[r].position, Ag.transform.position) <= Ag.stoppingDistance)
+        {
+            if (!Ag.hasPath || Ag.velocity.sqrMagnitude == 0f)
+            {
+                ReLoad();
+            }
+        }
+    }
+
+     
+}
